5f0fe90e062add7c5140712f210f8ea816155d4b
zh <zhaohan6357@163.com>
2018-07-06 10:47:24 +0800

    ch01

--- src/main/java/ch01/Main.java
+++ src/main/java/ch01/Main.java
@@ -1,7 +1,4 @@
 package ch01;
-import org.apache.commons.cli.*;
-
-import java.util.Arrays;
 
 /**
  * Hello world!
@@ -9,38 +6,17 @@ import java.util.Arrays;
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
-        // Create a Parser
-        CommandLineParser parser = new BasicParser( );
-
-        Options options = new Options( );
-        options.addOption("h", "help", false, "Print this usage information");
-        options.addOption("v", "verbose", false, "Print out VERBOSE information" );
-        options.addOption("f", "file", true, "File to save program output to");
-        // Parse the program arguments
-        CommandLine commandLine = null;
-        try {
-            commandLine = parser.parse( options, args );
-        } catch (ParseException e) {
-            e.printStackTrace();
+    public static void startJVM(Cmd cmd){
+        System.out.print("classpath: "+cmd.cpOpthion+" classname: "+cmd.className+" args:");
+        for(String str:cmd.arg){
+            System.out.print(str+" ");
         }
-        // Set the appropriate variables based on supplied options
-        boolean verbose = false;
-        String file = "";
-
-        if( commandLine.hasOption('h') ) {
-            System.out.println( "Help Message");
-            //System.exit(0);
-        }
-        if( commandLine.hasOption('v') ) {
-            verbose = true;
-        }
-        if( commandLine.hasOption('f') ) {
-            file = commandLine.getOptionValue('f');
-            String[] strs=commandLine.getArgs();
-            System.out.println(file);
-            System.out.println(Arrays.toString(strs));
+    }
+    public static void main( String[] args ) {
+        Cmd cmd=new Cmd();
+        cmd.cmdParse(args);
+        if(cmd.className!=null){
+           startJVM(cmd);
         }
     }
 }
---
+++ src/main/java/ch02/Main.java
@@ -0,0 +1,7 @@
+package ch02;
+
+public class Main {
+    public static void main(String[] args) {
+        Cmd c=new Cmd();
+    }
+}
